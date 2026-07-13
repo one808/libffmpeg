cb5788dd1248a067cb033edee70918dac5b1d317
one808 <90703614+one808@users.noreply.github.com>
2026-07-13 12:17:50 +0800

    ci: disable ASM for x265 cross-compilation

--- ci/build-libffmpeg-mingw32.sh
+++ ci/build-libffmpeg-mingw32.sh
@@ -48,13 +48,14 @@ fi
 cd "$DEPS/x265"
 mkdir -p build && cd build
 cmake -DCMAKE_SYSTEM_NAME=Windows \
-    -DCMAKE_C_COMPILER=${TARGET}-gcc \
-    -DCMAKE_CXX_COMPILER=${TARGET}-g++ \
+    -DCMAKE_C_COMPILER='${TARGET}-gcc' \
+    -DCMAKE_CXX_COMPILER='${TARGET}-g++' \
     -DCMAKE_INSTALL_PREFIX="$PREFIX" \
     -DCMAKE_BUILD_TYPE=Release \
     -DENABLE_CLI=OFF \
     -DENABLE_SHARED=OFF \
-    ../source -DCMAKE_SYSTEM_PROCESSOR=x86 -DCMAKE_ASM_NASM_FLAGS=-w
+    -DENABLE_ASM=OFF \
+    ../source
 make -j$JOBS
 make install
 cd ../../..
