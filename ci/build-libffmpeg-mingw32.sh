e2713f137329f1eae10ef891459bd823c2e38e36
one808 <90703614+one808@users.noreply.github.com>
2026-07-13 14:58:32 +0800

    ci: disable vorbis/fdk-aac in FFmpeg (build script still builds them). Fix pkg-config for cross-compilation

--- ci/build-libffmpeg-mingw32.sh
+++ ci/build-libffmpeg-mingw32.sh
@@ -98,8 +98,8 @@ EXTRA_LDFLAGS="-L$PREFIX/lib"
     --enable-libvpx \
     --enable-libmp3lame \
     --enable-libopus \
-    --enable-libvorbis \
-    --enable-libfdk-aac \
+    --disable-libvorbis \
+    --disable-libfdk-aac \
     --disable-openssl \
     --disable-libass \
     --disable-debug \
