45be7f86ee5d85092eb42e2be3475b9e1e916377
one808 <90703614+one808@users.noreply.github.com>
2026-07-13 14:33:34 +0800

    ci: use --pkg-config=pkg-config for cross-compilation

--- ci/build-libffmpeg-mingw32.sh
+++ ci/build-libffmpeg-mingw32.sh
@@ -78,6 +78,7 @@ EXTRA_CFLAGS="-I$PREFIX/include"
 EXTRA_LDFLAGS="-L$PREFIX/lib"
 
 ./configure \
+    --pkg-config=pkg-config \
     --target-os=mingw32 \
     --arch=x86 \
     --cross-prefix=${TARGET}- \
