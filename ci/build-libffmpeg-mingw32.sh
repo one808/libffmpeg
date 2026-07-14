33650d606f079350f48885f670c335454a1b8018
one808 <90703614+one808@users.noreply.github.com>
2026-07-14 16:14:31 +0800

    ci: fix openh264 - use meson instead of cmake (no CMakeLists.txt)

--- ci/build-libffmpeg-mingw32.sh
+++ ci/build-libffmpeg-mingw32.sh
@@ -148,28 +148,9 @@ make -j$JOBS
 make install
 cd "$WORK_DIR"
 
-# 4. openh264 (Cisco H.264)
-echo "=== Building openh264 ==="
-if [ ! -d "$DEPS/openh264" ]; then
-    git clone --depth 1 https://github.com/cisco/openh264.git "$DEPS/openh264"
-fi
-mkdir -p "$DEPS/openh264_build"
-cd "$DEPS/openh264_build"
-cmake "$DEPS/openh264" \
-    -DCMAKE_SYSTEM_NAME=Windows \
-    -DCMAKE_SYSTEM_PROCESSOR=x86 \
-    -DCMAKE_C_COMPILER=${TARGET}-gcc \
-    -DCMAKE_CXX_COMPILER=${TARGET}-g++ \
-    -DCMAKE_RC_COMPILER=${TARGET}-windres \
-    -DCMAKE_INSTALL_PREFIX="$PREFIX" \
-    -DBUILD_SHARED_LIBS=OFF \
-    -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
-    -DENABLE_SVC=OFF \
-    -DENABLE_CLI=OFF \
-    -DCMAKE_ASM_NASM_COMPILER=nasm 2>&1 | tail -5
-make -j$JOBS
-make install
-cd "$WORK_DIR"
+# 4. openh264 (Cisco H.264) - uses meson
+build_meson "openh264" "https://github.com/cisco/openh264.git" \
+    "-Dtests=disabled -Dexamples=disabled -Dcli=disabled"
 
 # 5. vid.stab (video stabilization)
 build_cmake "vid.stab" "https://github.com/georgmartius/vid.stab.git" \
