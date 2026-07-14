ffa998df6295990c92184ab1598b8da68a10f88b
one808 <90703614+one808@users.noreply.github.com>
2026-07-14 14:27:43 +0800

    ci: add libass (freetype+fribidi+libass), enable ALL 10 external libs

--- ci/build-libffmpeg-mingw32.sh
+++ ci/build-libffmpeg-mingw32.sh
@@ -46,6 +46,41 @@ build_dep() {
     cd "$WORK_DIR"
 }
 
+# Helper: build cmake project
+build_cmake() {
+    local name=$1 url=$2 cmake_args=$3
+    echo "=== Building $name (cmake) ==="
+    if [ ! -d "$DEPS/$name" ]; then
+        git clone --depth 1 "$url" "$DEPS/$name"
+    fi
+    mkdir -p "$DEPS/${name}_build"
+    cd "$DEPS/${name}_build"
+    cmake "$DEPS/$name" \
+        -DCMAKE_SYSTEM_NAME=Windows \
+        -DCMAKE_C_COMPILER=${TARGET}-gcc \
+        -DCMAKE_CXX_COMPILER=${TARGET}-g++ \
+        -DCMAKE_RC_COMPILER=${TARGET}-windres \
+        -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+        -DBUILD_SHARED_LIBS=OFF \
+        -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+        $cmake_args 2>&1 | tail -5
+    make -j$JOBS
+    make install
+    cd "$WORK_DIR"
+}
+
+# 1. freetype2 (needed by libass)
+build_cmake "freetype" "https://github.com/freetype/freetype.git" \
+    "-DFT_DISABLE_HARFBUZZ=ON -DFT_DISABLE_BROTLI=ON"
+
+# 2. fribidi (needed by libass)
+build_cmake "fribidi" "https://github.com/fribidi/fribidi.git" \
+    "-DFRIBIDI_TEST=OFF -DBUILD_SHARED_LIBS=OFF"
+
+# 3. libass
+build_dep "libass" "https://github.com/libass/libass.git" \
+    "--disable-fontconfig --disable-harfbuzz --disable-coretext"
+
 # 1. x264
 build_dep "x264" "https://code.videolan.org/videolan/x264.git" \
     "--enable-pic --enable-static --disable-cli --disable-lavf"
@@ -174,7 +209,7 @@ EXTRA_LDFLAGS="-L$PREFIX/lib"
     --enable-libvorbis \
     --enable-libfdk-aac \
     --enable-openssl \
-    --disable-libass \
+    --enable-libass \
     --disable-debug \
     --disable-doc \
     --disable-programs \
